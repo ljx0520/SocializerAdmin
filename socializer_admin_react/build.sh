a4207102c3d31001edf73b8b428685b8a50dc2ba
Joseph Li <ljx930520@gmail.com>
2022-08-01 01:28:29 +1000

    build(react): build --no-cache

--- socializer_admin_react/build.sh
+++ socializer_admin_react/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo docker build -t socializer_admin_react:latest .
+sudo docker build --no-cache -t socializer_admin_react:latest .
 
 docker tag socializer_admin_react registry.digitalocean.com/socialzer/socializer_admin_react
 
